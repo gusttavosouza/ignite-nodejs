6842c95bfa45777c326558a3c9da934490360b47
gusttavosouza <gustavorobertotb@outlook.com>
2023-06-14 17:12:29 -0300

    feat: init config prisma

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import { PrismaClient } from 'prisma/prisma-client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Gustavo Roberto de Souza',
+    email: 'gustavorobertotb@outlook.com',
+  },
+});
